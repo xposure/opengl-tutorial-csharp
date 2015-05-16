30326cc4a81292dbb4c6d3db5a9cbff3a1219e20
Chris Phillips <chris.phillips@mindmatrix.net>
2015-05-16 18:15:29 -0400

    t2 cleanup

--- T2 - The VAO/Program.cs
+++ T2 - The VAO/Program.cs
@@ -31,9 +31,16 @@ namespace T2___The_VAO
                     0.0f,   1.0f, 0.0f,
                 };
 
+                // This will identify our vertex buffer
                 var vertexBuffer = GL.GenBuffer();
+                
+                // Generate 1 buffer, put the resulting identifier in vertexbuffer
                 GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
-                GL.BufferData(BufferTarget.ArrayBuffer, new IntPtr(sizeof(float) * g_vertex_buffer_data.Length), g_vertex_buffer_data, BufferUsageHint.StaticDraw);
+
+                // The following commands will talk about our 'vertexbuffer' buffer
+                var vertexBufferSize = new IntPtr(sizeof(float) * g_vertex_buffer_data.Length);
+                GL.BufferData(BufferTarget.ArrayBuffer, vertexBufferSize, g_vertex_buffer_data, 
+                                BufferUsageHint.StaticDraw);
 
                 var programID = LoadShaders("simple.vert", "simple.frag");
 
@@ -45,15 +52,28 @@ namespace T2___The_VAO
 
                     GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
 
+                    // Use our shader
                     GL.UseProgram(programID);
 
                     GL.EnableVertexAttribArray(0);
                     GL.BindBuffer(BufferTarget.ArrayBuffer, vertexBuffer);
-                    GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
-                    GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-                    GL.DisableVertexAttribArray(0);
-                    // Swap buffers
+                    GL.VertexAttribPointer(
+                        0,                             // attribute 0. No particular reason for 0, 
+                                                       // but must match the layout in the shader.
+                        3,                             // size
+                        VertexAttribPointerType.Float, // type
+                        false,                         // normalized?
+                        0,                             // stride
+                        0                              // array buffer offset
+                    );
 
+                    // draw the triangles
+                    // Starting from vertex 0; 3 vertices total -> 1 triangle
+                    GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+                    GL.DisableVertexAttribArray(0);
+
+                    // Swap buffers
                     game.SwapBuffers();
                     game.ProcessEvents();
 
@@ -65,57 +85,57 @@ namespace T2___The_VAO
             }
         }
 
-        private static int LoadShaders(string vertexPath, string fragmentPath)
+private static int LoadShaders(string vertexPath, string fragmentPath)
+{
+    var vertexID = GL.CreateShader(ShaderType.VertexShader);
+    var fragmentID = GL.CreateShader(ShaderType.FragmentShader);
+    try
+    {
+        LoadShader(vertexID, vertexPath, ShaderType.VertexShader);
+        LoadShader(fragmentID, fragmentPath, ShaderType.FragmentShader);
+
+        Console.WriteLine("Linking program {0}, {1}", vertexPath, fragmentPath);
+        var programID = GL.CreateProgram();
+        GL.AttachShader(programID, vertexID);
+        GL.AttachShader(programID, fragmentID);
+        GL.LinkProgram(programID);
+
+        var result = 0;
+        GL.GetProgram(programID, GetProgramParameterName.LinkStatus, out result);
+        if (result != 1)
         {
-            var vertexID = GL.CreateShader(ShaderType.VertexShader);
-            var fragmentID = GL.CreateShader(ShaderType.FragmentShader);
-            try
-            {
-                LoadShader(vertexID, vertexPath, ShaderType.VertexShader);
-                LoadShader(fragmentID, fragmentPath, ShaderType.FragmentShader);
-
-                Console.WriteLine("Linking program {0}, {1}", vertexPath, fragmentPath);
-                var programID = GL.CreateProgram();
-                GL.AttachShader(programID, vertexID);
-                GL.AttachShader(programID, fragmentID);
-                GL.LinkProgram(programID);
-
-                var result = 0;
-                GL.GetProgram(programID, GetProgramParameterName.LinkStatus, out result);
-                if (result != 1)
-                {
-                    var info = GL.GetProgramInfoLog(programID);
-                    Console.WriteLine("FAILED: Linking program with result: {0}, info: {1}", result, info);
-                }
-
-                return programID;
-            }
-            finally
-            {
-                GL.DeleteShader(vertexID);
-                GL.DeleteShader(fragmentID);
-            }
+            var info = GL.GetProgramInfoLog(programID);
+            Console.WriteLine("FAILED: Linking program with result: {0}, info: {1}", result, info);
         }
 
-        private static void LoadShader(int shaderID, string path, ShaderType shaderType)
+        return programID;
+    }
+    finally
+    {
+        GL.DeleteShader(vertexID);
+        GL.DeleteShader(fragmentID);
+    }
+}
+
+private static void LoadShader(int shaderID, string path, ShaderType shaderType)
+{
+    using (var sr = new StreamReader(path))
+    {
+        //var shaderID = GL.CreateShader(shaderType);
+        Console.WriteLine("Compiling shader {0}:{1}", shaderType, path);
+
+        var code = sr.ReadToEnd();
+        GL.ShaderSource(shaderID, code);
+        GL.CompileShader(shaderID);
+
+        var result = 0;
+        GL.GetShader(shaderID, ShaderParameter.CompileStatus, out result);
+        if (result != 1)
         {
-            using (var sr = new StreamReader(path))
-            {
-                //var shaderID = GL.CreateShader(shaderType);
-                Console.WriteLine("Compiling shader {0}:{1}", shaderType, path);
-
-                var code = sr.ReadToEnd();
-                GL.ShaderSource(shaderID, code);
-                GL.CompileShader(shaderID);
-
-                var result = 0;
-                GL.GetShader(shaderID, ShaderParameter.CompileStatus, out result);
-                if (result != 1)
-                {
-                    var info = GL.GetShaderInfoLog(shaderID);
-                    Console.WriteLine("FAILED: Compiling shader {0}:{1} with result: {2}, info: {3}", shaderType, path, result, info);
-                }
-            }
+            var info = GL.GetShaderInfoLog(shaderID);
+            Console.WriteLine("FAILED: Compiling shader {0}:{1} with result: {2}, info: {3}", shaderType, path, result, info);
         }
+    }
+}
     }
 }
